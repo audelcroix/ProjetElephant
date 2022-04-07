e550d38aba6533322b16f6b0207dcbc19edf48d7
audelcroix <au.delcroix@live.fr>
2022-04-07 17:55:43 +0200

    begin deployment

---
+++ client/build/precache-manifest.908548a94aca5d3aa5733ca1f86cf704.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3ceddbaaa65a2a2aec345da3d35c87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a29f467fbcc89832e840",
+    "url": "/static/css/2.e3b0196e.chunk.css"
+  },
+  {
+    "revision": "97aca92714d3fd5e0b8e",
+    "url": "/static/css/main.93670b02.chunk.css"
+  },
+  {
+    "revision": "a29f467fbcc89832e840",
+    "url": "/static/js/2.9edefd85.chunk.js"
+  },
+  {
+    "revision": "2730afdb1eb01798e6cd324e733c03f3",
+    "url": "/static/js/2.9edefd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97aca92714d3fd5e0b8e",
+    "url": "/static/js/main.67b7db55.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
