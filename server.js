f5d1fc96a369807f8e40289cef945f80729a58b0
audelcroix <au.delcroix@live.fr>
2022-04-07 18:00:40 +0200

    update parameter

--- server.js
+++ server.js
@@ -56,10 +56,16 @@ app.use("/api/collections", require("./routes/collectionRoutes"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // set static folder
-  app.use(express.static("client/build"));
+  /* app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  }); */
+
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+  app.get("*", function (request, response) {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
   });
 }
 
